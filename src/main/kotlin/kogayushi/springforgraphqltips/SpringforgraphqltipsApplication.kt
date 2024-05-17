5023a9f722d6207dce5aa9c5c6e884d9b04e6f11
koga yushi <youshe.old.pleasure@gmail.com>
2024-05-18 00:05:28 +0900

    set up

---
+++ src/main/kotlin/kogayushi/springforgraphqltips/SpringforgraphqltipsApplication.kt
@@ -0,0 +1,11 @@
+package kogayushi.springforgraphqltips
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringforgraphqltipsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringforgraphqltipsApplication>(*args)
+}
