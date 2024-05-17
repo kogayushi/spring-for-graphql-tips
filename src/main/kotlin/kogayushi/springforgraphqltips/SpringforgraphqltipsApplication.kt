be1ae8c3d9daf67e19d1c4e1a9d45e499ba35130
koga yushi <youshe.old.pleasure@gmail.com>
2024-05-18 00:14:20 +0900

    format

--- src/main/kotlin/kogayushi/springforgraphqltips/SpringforgraphqltipsApplication.kt
+++ src/main/kotlin/kogayushi/springforgraphqltips/SpringforgraphqltipsApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class SpringforgraphqltipsApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringforgraphqltipsApplication>(*args)
+    runApplication<SpringforgraphqltipsApplication>(*args)
 }
--- src/test/kotlin/kogayushi/springforgraphqltips/SpringforgraphqltipsApplicationTests.kt
+++ src/test/kotlin/kogayushi/springforgraphqltips/SpringforgraphqltipsApplicationTests.kt
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class SpringforgraphqltipsApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
